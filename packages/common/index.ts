30231c836d3e088c1bb5f3363abeda2560e1b3ba
Pranil <pranildhutraj@gmail.com>
2025-12-22 19:56:23 +0530

    types added for signin, signup, create workflow and update workflow

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,1 +1,2 @@
 export * from "./metadata/index";
+export * from "./types/index";
---
+++ packages/common/types/index.ts
@@ -0,0 +1,62 @@
+import { z } from "zod";
+
+export const SignUpSchema = z.object({
+  username: z.string().min(3).max(100),
+  password: z.string().min(6).max(20),
+});
+
+export const SignInSchema = z.object({
+  username: z.string().min(3).max(100),
+  password: z.string().min(6).max(20),
+});
+
+export const CreateWorkflowSchema = z.object({
+  nodes: z.array(
+    z.object({
+      nodeId: z.string(),
+      data: z.object({
+        kind: z.enum(["ACTION", "TRIGGER"]),
+        metadata: z.any(),
+      }),
+      credentials: z.any(),
+      id: z.string(),
+      type: z.string(),
+      position: z.object({
+        x: z.number(),
+        y: z.number(),
+      }),
+    })
+  ),
+  edges: z.array(
+    z.object({
+      id: z.string(),
+      source: z.string(),
+      target: z.string(),
+    })
+  ),
+});
+
+export const UpdateWorkflowSchema = z.object({
+  nodes: z.array(
+    z.object({
+      nodeId: z.string(),
+      data: z.object({
+        kind: z.enum(["ACTION", "TRIGGER"]),
+        metadata: z.any(),
+      }),
+      credentials: z.any(),
+      id: z.string(),
+      position: z.object({
+        x: z.number(),
+        y: z.number(),
+      }),
+    })
+  ),
+  edges: z.array(
+    z.object({
+      id: z.string(),
+      source: z.string(),
+      target: z.string(),
+    })
+  ),
+});
